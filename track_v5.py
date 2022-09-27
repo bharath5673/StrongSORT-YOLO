42212284a9d8f54220abaf82ae2ff55063ee5a72
bharath <33729709+bharath5673@users.noreply.github.com>
2022-09-27 16:23:37 +0530

    Update track_v5.py

--- track_v5.py
+++ track_v5.py
@@ -63,7 +63,7 @@ def run(
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        show_vid=False,  # show results
+        show_vid=True,  # show results
         save_txt=False,  # save results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
         save_crop=False,  # save cropped prediction boxes
