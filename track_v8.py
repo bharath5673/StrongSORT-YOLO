702f58c5c7a596f2ca39d149306afa19b792c1e5
bharath <33729709+bharath5673@users.noreply.github.com>
2023-12-30 20:27:18 +0530

    Update track_v8.py
    Multistreaming

--- track_v8.py
+++ track_v8.py
@@ -9,7 +9,7 @@ import time
 from collections import Counter, deque
 import pandas as pd
 import argparse
-from multiprocessing import Process
+from multiprocessing import Pool
 
 # Load a model
 model = YOLO('yolov8n-seg.pt')  # load an official model
@@ -20,19 +20,21 @@ model.overrides['max_det'] = 1000  # maximum number of detections per image
 # model.overrides['classes'] = 2 ## define classes
 names = model.names
 names = {value: key for key, value in names.items()}
-
+colors = np.random.randint(0, 255, size=(80, 3), dtype='uint8')
 
 
 tracking_trajectories = {}
 def process(image, track=True):
     global input_video_name
     bboxes = []
+    frameId = 0
     # Place this code outside the loop to avoid creating the file multiple times
     if not os.path.exists('output'):
         os.makedirs('output')
+    labels_file_path = os.path.abspath(f'./output/{input_video_name}_labels.txt')
 
     # Open the file in 'a' (append) mode
-    with open('output/'+input_video_name+'_labels.txt', 'a') as file:
+    with open(labels_file_path, 'a') as file:
         if track is True:
             results = model.track(image, verbose=False, device=0, persist=True, tracker="botsort.yaml")
 
@@ -80,10 +82,21 @@ def process(image, track=True):
                         polygon = mask
                         cv2.polylines(image, [np.int32(polygon)], True, (255, 0, 0), thickness=2)
 
+                        color_ = [int(c) for c in colors[int(classes)]]
+                        # cv2.fillPoly(image, [np.int32(polygon)], color_) 
+                        mask = image.copy()
+                        cv2.fillPoly(mask, [np.int32(polygon)], color_) 
+                        alpha = 0.5  # Adjust the transparency level
+                        blended_image = cv2.addWeighted(image, 1 - alpha, mask, alpha, 0)
+                        image = blended_image.copy()
+
+
         for item in bboxes:
             bbox_coords, scores, classes, *id_ = item if len(item) == 4 else (*item, None)
             line = f'{frameId} {int(classes)} {int(id_[0])} {round(float(scores), 3)} {int(bbox_coords[0])} {int(bbox_coords[1])} {int(bbox_coords[2])} {int(bbox_coords[3])} -1 -1 -1 -1\n'
+            # print(line)
             file.write(line)
+
 
     if not track:
         results = model.predict(image, verbose=False, device=0)  # predict on an image
@@ -112,27 +125,38 @@ def process(image, track=True):
                     polygon = mask
                     cv2.polylines(image, [np.int32(polygon)], True, (255, 0, 0), thickness=2)
 
+                    color_ = [int(c) for c in colors[int(classes)]]
+                    # cv2.fillPoly(image, [np.int32(polygon)], color_) 
+                    mask = image.copy()
+                    cv2.fillPoly(mask, [np.int32(polygon)], color_) 
+                    alpha = 0.5  # Adjust the transparency level
+                    blended_image = cv2.addWeighted(image, 1 - alpha, mask, alpha, 0)
+                    image = blended_image.copy()
+
+
     return image
 
 
 
-def process_video(input_path, output_path, track, count):
-    global input_video_name, frameId
-    cap = cv2.VideoCapture(int(input_path) if input_path == '0' else input_path)
+def process_video(source):
+    global input_video_name
+    cap = cv2.VideoCapture(int(source) if source == '0' else source)
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    input_video_name = input_path.split('.')[0]
-    out = cv2.VideoWriter('output/'+output_path, fourcc, fps, (frame_width, frame_height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Change the codec if needed (e.g., 'XVID')
+    # input_video_name = source.split('.')[0]  # Get the input video name without extension
+    input_video_name = os.path.splitext(os.path.basename(source))[0]
+    # print('testing : ', input_video_name)
+    out = cv2.VideoWriter(f'output/{input_video_name}_output.mp4', fourcc, 15, (frame_width, frame_height))
 
     if not cap.isOpened():
-        print(f"Error: Could not open video file: {input_path}")
+        print(f"Error: Could not open video file {source}.")
         return
 
     frameId = 0
     start_time = time.time()
-    fps_str = ""
+    fps_str = str()
 
     while True:
         frameId += 1
@@ -141,87 +165,78 @@ def process_video(input_path, output_path, track, count):
         if not ret:
             break
 
-        frame = process(frame1, track)
+        frame = process(frame1, args.track)
 
-        if not track and count:
+        if not args.track and args.count:
             print('[INFO] count works only when objects are tracking.. so use: --track --count')
             break
 
-        if track and count:
-            item_dict = {}
+        if args.track and args.count:
+            itemDict={}
+            ## NOTE: this works only if save-txt is true
             try:
-                df = pd.read_csv(f'output/{input_video_name}_labels.txt', header=None, delim_whitespace=True)
-                df = df.iloc[:, 0:3]
-                df.columns = ["frameid", "class", "trackid"]
-                df = df[['class', 'trackid']]
+                df = pd.read_csv('output/'+input_video_name+'_labels.txt' , header=None, delim_whitespace=True)
+                # print(df)
+                df = df.iloc[:,0:3]
+                df.columns=["frameid" ,"class","trackid"]
+                df = df[['class','trackid']]
                 df = (df.groupby('trackid')['class']
-                        .apply(list)
-                        .apply(lambda x: sorted(x))
-                        ).reset_index()
-                df['class'] = df['class'].apply(lambda x: Counter(x).most_common(1)[0][0])
+                          .apply(list)
+                          .apply(lambda x:sorted(x))
+                         ).reset_index()
+                df['class']=df['class'].apply(lambda x: Counter(x).most_common(1)[0][0])
                 vc = df['class'].value_counts()
                 vc = dict(vc)
 
                 vc2 = {}
                 for key, val in enumerate(names):
                     vc2[key] = val
-                item_dict = dict((vc2[key], value) for (key, value) in vc.items())
-                item_dict = dict(sorted(item_dict.items(), key=lambda item: item[0]))
+                itemDict = dict((vc2[key], value) for (key, value) in vc.items())
+                itemDict  = dict(sorted(itemDict.items(), key=lambda item: item[0]))
+                # print(itemDict)
+
             except:
                 pass
 
+            ## overlay
             display = frame.copy()
             h, w = frame.shape[0], frame.shape[1]
-            x1, y1, x2, y2 = 10, 10, 10, 70
-            txt_size = cv2.getTextSize(str(item_dict), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)[0]
-            cv2.rectangle(frame, (x1, y1 + 1), (txt_size[0] * 2, y2), (0, 0, 0), -1)
-            cv2.putText(frame, '{}'.format(item_dict), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (210, 210, 210), 2)
+            x1, y1, x2, y2 =10, 10, 10, 70
+            txt_size = cv2.getTextSize(str(itemDict), cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)[0]
+            cv2.rectangle(frame, (x1, y1 + 1), (txt_size[0] * 2, y2),(0, 0, 0),-1)
+            cv2.putText(frame, '{}'.format(itemDict), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_SIMPLEX,0.7, (210, 210, 210), 2)
             cv2.addWeighted(frame, 0.7, display, 1 - 0.7, 0, frame)
 
         current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
         if frameId % 10 == 0:
             end_time = time.time()
             elapsed_time = end_time - start_time
-            fps_current = 10 / elapsed_time
+            fps_current = 10 / elapsed_time  # Calculate FPS over the last 20 frames
             fps_str = f'FPS: {fps_current:.2f}'
-            start_time = time.time()
+            start_time = time.time()  # Reset start_time for the next 20 frames
 
         cv2.putText(frame, fps_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1, cv2.LINE_AA)
-        cv2.imshow("yolo", frame)
+
+        cv2.imshow(f"yolo_{source}", frame)
         out.write(frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    # Release the video capture and writer
     cap.release()
     out.release()
     cv2.destroyAllWindows()
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process multiple videos with YOLO.')
-    parser.add_argument('--sources', nargs='+', required=True, help='Input video file paths or camera indices')
+
+    parser = argparse.ArgumentParser(description='Process video with YOLO.')
+    parser.add_argument('--source', nargs='+', type=str, default='0', help='Input video file paths or camera indices')
     parser.add_argument('--track', action='store_true', help='if track objects')
     parser.add_argument('--count', action='store_true', help='if count objects')
 
     args = parser.parse_args()
 
-    if len(args.sources) == 0:
-        print("Error: Please provide at least one source.")
-        exit()
-
-    if args.track and not args.count:
-        print("Warning: Tracking is enabled, but counting is not. Counting works when tracking is enabled.")
-
-    processes = []
-
-    for source in args.sources:
-        output_video_name = source.split('.')[0] + '_output.mp4'
-        p = Process(target=process_video, args=(source, output_video_name, args.track, args.count))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
-
+    with Pool(processes=len(args.source)) as pool:
+        pool.map(process_video, args.source)
 
